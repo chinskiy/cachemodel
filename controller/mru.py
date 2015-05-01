57c49c1bcea9ba3b735ccf9a410c0952746dbbea
chinskiy <chinskiy93@gmail.com>
2015-05-02 02:33:59 +0300

    add mru/bad cachehit rate for my alg

--- controller/mru.py
+++ controller/mru.py
@@ -4,6 +4,24 @@ import cachestorage
 class MRU():
     def __init__(self, lencache):
         self.cachestorage = cachestorage.Cachemem(lencache)
+        self.start, self.lencache = 0, lencache
+        self.lastadd = -1
 
     def makestep(self, adress):
-        pass
+        index = self.cachestorage.findindexaddr(adress)
+        if self.start < self.lencache:
+            if index == -1:
+                self.cachestorage.addmemfromds(self.start, adress)
+                self.lastadd = self.start
+                self.start += 1
+                return 0
+            else:
+                self.lastadd = index
+                return 1
+        else:
+            if index == -1:
+                self.cachestorage.addmemfromds(self.lastadd, adress)
+                return 0
+            else:
+                self.lastadd = index
+                return 1
