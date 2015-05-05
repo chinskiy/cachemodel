78aa7184a7f74002eaa652b4ebf8e5654ea872f4
chinskiy <chinskiy93@gmail.com>
2015-05-05 16:05:39 +0300

    add slru, change a few lru

--- controller/lru.py
+++ controller/lru.py
@@ -8,12 +8,13 @@ class LRU():
         self.start, self.lencache = 0, lencache
         self.cacheused = np.asarray([0 for _ in range(lencache)])
 
-    def makestep(self, adress):
+    def makestep(self, adress, retmemid=0):
         index = self.cachestorage.findindexaddr(adress)
         if self.start < self.lencache:
             if index == -1:
                 self.cachestorage.addmemfromds(self.start, adress)
                 self.cacheused += 1
+                self.cacheused[self.start] = 0
                 self.start += 1
                 return 0
             else:
@@ -23,10 +24,17 @@ class LRU():
         else:
             if index == -1:
                 maxind = np.where(self.cacheused == np.max(self.cacheused))[0][0]
-                self.cachestorage.addmemfromds(maxind, adress)
-                self.cacheused += 1
-                self.cacheused[maxind] = 0
-                return 0
+                if retmemid == 0:
+                    self.cachestorage.addmemfromds(maxind, adress)
+                    self.cacheused += 1
+                    self.cacheused[maxind] = 0
+                    return 0
+                else:
+                    tmp = self.cachestorage.memid[maxind]
+                    self.cachestorage.addmemfromds(maxind, adress)
+                    self.cacheused += 1
+                    self.cacheused[maxind] = 0
+                    return tmp
             else:
                 self.cacheused += 1
                 self.cacheused[index] = 0
--- controller/slru.py
+++ controller/slru.py
@@ -1,6 +1,29 @@
-import cachestorage
+import controller.lru as lru
+import numpy as np
 
 
 class SLRU():
     def __init__(self, lencache):
-        pass
+        self.segment = [lru.LRU(lencache // 2), lru.LRU(lencache // 2)]
+
+    def makestep(self, adress):
+        index = [self.segment[0].cachestorage.findindexaddr(adress),
+                 self.segment[1].cachestorage.findindexaddr(adress)]
+
+        if index[0] == -1:
+            if index[1] == -1:
+                self.segment[0].makestep(adress)
+                return 0
+            elif index[1] != -1:
+                self.segment[1].makestep(adress)
+                return 1
+        elif index[0] != -1:
+            if index[1] == -1:
+                tmp = self.segment[1].makestep(adress, 1)
+                self.segment[0].cachestorage.memid[index[0]] = tmp
+                self.segment[0].cacheused += 1
+                if tmp == 0:
+                    self.segment[0].cacheused[index[0]] = np.max(self.segment[0].cacheused) + 1
+                else:
+                    self.segment[0].cacheused[index[0]] = 0
+                return 1
