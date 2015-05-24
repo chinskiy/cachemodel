53d4672c7346e4c5db5db299328a71010a0cca55
chinskiy <chinskiy93@gmail.com>
2015-05-24 12:24:42 +0300

    best rl variance for now/ using cache adresses and request as state

--- controller/rl.py
+++ controller/rl.py
@@ -1,9 +1,10 @@
 import cachestorage
 import numpy as np
 import random
+import copy
 
 class RL:
-    def __init__(self, lencache, epsilon=0.1, alpha=0.2, gamma=0.9):
+    def __init__(self, lencache, epsilon=0.2, alpha=0.2, gamma=0.9):
         self.cachestorage = cachestorage.Cachemem(lencache)
         self.start, self.lencache = 0, lencache
         self.epsilon, self.alpha, self.gamma = epsilon, alpha, gamma
@@ -14,39 +15,46 @@ class RL:
         self.reward = 0
 
     def makestep(self, adress):
-
         index = self.cachestorage.findindexaddr(adress)
         if self.start < self.lencache:
             if index == -1:
                 self.cachestorage.addmemfromds(self.start, adress)
-                self.cacheused += 1
-                self.cacheused[self.start] = 0
+                # self.cacheused += 1
+                # self.cacheused[self.start] = 0
                 self.start += 1
                 return 0
             else:
-                self.cacheused += 1
-                self.cacheused[index] = 0
-                self.cachefreq[index] += 1
+                # self.cacheused += 1
+                # self.cacheused[index] = 0
+                # self.cachefreq[index] += 1
                 return 1
         else:
             if index == -1:
+                tmp = copy.deepcopy(self.cachestorage.memid)
+                np.append(tmp, adress)
+                tmp.sort()
                 if self.stateold != '0':
-                    self.learn(self.stateold, self.actionold, self.reward, tuple(self.cachefreq))
+                    self.learn(self.stateold, self.actionold, self.reward, tuple(tmp))
 
-                action = self.choose_action(tuple(self.cachefreq), 0)
+                action = self.choose_action(tuple(tmp), 0)
                 self.cachestorage.addmemfromds(action, adress)
-                self.cachefreq[action] = 0
-                self.cacheused += 1
-                self.cacheused[action] = 0
 
-                self.stateold, self.actionold = tuple(self.cachefreq), action
+                # self.cachefreq[action] = 0
+                # self.cacheused += 1
+                # self.cacheused[action] = 0
+                tmp = copy.deepcopy(self.cachestorage.memid)
+                np.append(tmp, adress)
+                tmp.sort()
+
+                self.stateold, self.actionold = tuple(tmp), action
+
                 self.reward = 0
                 return 0
             else:
-                self.cachefreq[index] += 1
-                self.cacheused += 1
-                self.cacheused[index] = 0
-                self.reward += 1
+                # self.cachefreq[index] += 1
+                # self.cacheused += 1
+                # self.cacheused[index] = 0
+                self.reward += 2
                 return 1
 
     def choose_action(self, state, noact=1):
@@ -54,6 +62,7 @@ class RL:
             action = random.choice(self.actions) if noact != 0 else random.choice(self.actions[:-1])
         else:
             q = [self.getQ(state, a) for a in self.actions]
+            #print(q)
             maxq = max(q)
             if q.count(maxq) > 1:
                 best = [i for i in range(len(self.actions)) if q[i] == maxq]
@@ -68,7 +77,6 @@ class RL:
 
     def learn(self, state1, action1, reward, state2):
         maxqnew = max([self.getQ(state2, a) for a in self.actions])
-
         self.learnQ(state1, action1, reward, reward + self.gamma*maxqnew)
 
     def learnQ(self, state, action, reward, value):
